08f710ff29fe2068262d0349833a2b7c3fdba4a3
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 11:23:43 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
