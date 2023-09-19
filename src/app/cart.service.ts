ec661fb665d6bd82d35f604d91e0193087ed408d
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 11:05:12 -0500

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,4 +8,17 @@ export class CartService {
 
   items: Product[] = [];
   constructor() { }
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
 }
