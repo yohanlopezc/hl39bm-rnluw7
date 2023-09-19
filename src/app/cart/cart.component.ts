3b9a7571601dbb5240195e426ee77411a79ae88a
yohanlopezc <75345853+yohanlopezc@users.noreply.github.com>
2023-09-19 13:30:14 -0500

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,6 +12,7 @@ export class CartComponent {
   items = this.cartService.getItems();
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
 }
